c53ce027f98ffe5577545040710c07dbae43223d
Wilgeo·F·Lunatic <wilgeoflunatic@gmail.com>
2026-07-31 13:09:06 -0400

    Player move initial and simple scenes initial.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float stopDistance = 0.1f;
+    public Transform holdPoint;
+    //public InputSystem_Actions inputControl;
+    private PlayerInput _playerInput;
+    private InputAction _moveAction;
+
+    public Vector2 inputDirection;
+    public Vector2 mouseInputDirection;
+    private Vector3 mousePos;
+    public Vector2 facingDirection { get; private set; } = Vector2.right;
+    private Rigidbody2D rb;
+    private Vector3 vector3Meta = Vector3.one;
+    private Vector2 vector2Meta = Vector2.one;
+    private Vector3 holdPointPos = new Vector3(1, 0, 0);
+    //private Animator anim;
+    private bool useMouse = false;
+    void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _moveAction = _playerInput.actions["Move"];
+
+        rb = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
+
+        if (_playerInput.currentControlScheme == "Mouse")
+        {
+            useMouse = true;
+        }
+
+
+    }
+    void Update()
+    {
+
+        inputDirection = _moveAction.ReadValue<Vector2>();
+
+        //SetAnim();
+    }
+
+    void FixedUpdate()
+    {
+        HandleMove();
+    }
+    /*
+    void SetAnim()
+    {
+        anim.SetFloat("velocityX", Mathf.Abs(rb.linearVelocity.x) + Mathf.Abs(rb.linearVelocityY));
+        anim.SetBool("isHold", GetComponent<PlayerInteraction>().carryingChest != null ? true : false);
+    }
+    */
+    private void HandleMove()
+    {
+        //Mouse Move Solution
+        if (useMouse)
+        {
+
+            mousePos = Camera.main.ScreenToWorldPoint(new Vector3(inputDirection.x, inputDirection.y, Mathf.Abs(Camera.main.transform.position.z)));
+
+            mouseInputDirection = (mousePos - transform.position).normalized;
+
+            if ((mousePos - transform.position).magnitude < stopDistance)
+            {
+                rb.linearVelocity = Vector2.zero;
+                return;
+            }
+            rb.linearVelocity = mouseInputDirection * speed;
+
+            facingDirection = mouseInputDirection.normalized;
+            if (mouseInputDirection.x > 0)
+            {
+                vector3Meta.x = 1;
+            }
+            else if (mouseInputDirection.x < 0)
+            {
+                vector3Meta.x = -1;
+            }
+            transform.localScale = vector3Meta;
+        }
+        //Keyboard and Gamepad Move Solution
+        else
+        {
+            //rb.linearVelocity = inputDirection * speed;
+            vector2Meta.x = inputDirection.x * speed;
+            vector2Meta.y = rb.linearVelocityY;
+            rb.linearVelocity = vector2Meta;
+            if (inputDirection.x > 0)
+            {
+                vector3Meta.x = 1;
+                facingDirection = Vector2.right;
+            }
+            else if (inputDirection.x < 0)
+            {
+                vector3Meta.x = -1;
+                facingDirection = Vector2.left;
+
+            }
+            else if (inputDirection.y > 0)
+            {
+                facingDirection = Vector2.up;
+            }
+            else if (inputDirection.y < 0)
+            {
+                facingDirection = Vector2.down;
+            }
+
+            if (inputDirection.y != 0 && inputDirection.x != 0)
+            {
+                facingDirection = inputDirection.normalized;
+            }
+
+            transform.localScale = vector3Meta;
+        }
+
+
+
+
+    }
+
+
+}
